1b64dbac84ea512fd38cf250621cbe0b518f4842
DS Bell <dsbell81@gmail.com>
2016-04-22 13:33:39 -0400

    updated to demonstrate package

--- hello-world.go
+++ hello-world.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/dsbell81/hello-world/calc"
+)
 
 func main() {
 	fmt.Printf("Hello, world.\n")
+
+	var x, y int = 10, 5
+	fmt.Println("x is:", x)
+	fmt.Println("y is:", y)
+	fmt.Println(calc.Add(x, y))
+	fmt.Println(calc.Subtract(x, y))
 }
